4d72ea7c7b8531ecadeeed82cdcb1b44b87c8fe9
trangtnt1509 <tunuthutrang836@gmail.com>
2025-06-04 21:06:52 +0700

    sua anh và form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -34,7 +34,7 @@
                                             <div class="body-text1">Fill in the information below to add a new account</div>
                                             <div class="avatar-upload mt-3 mb-4">
                                                 <label for="avatarInput">
-                                                    <img src="{{ asset('default-avatar.png') }}" alt="Avatar" class="avatar-preview" id="avatarPreview">
+                                                    <img src="{{ asset('images/images.jpg') }}" alt="Avatar" class="avatar-preview" id="avatarPreview">
                                                 </label>
                                                 <input type="file" id="avatarInput" name="avatar" accept="image/*" style="display: none;">
                                             </div>                                       
@@ -72,68 +72,34 @@
                                             <div class="body-text">Items that the account is allowed to edit</div>
                                         </div>
                                         <div class="right flex-grow">
+                                           
                                             <fieldset class="mb-24">
-                                                <div class="body-title mb-10">Add product</div>
-                                                <div class="radio-buttons">
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="add-product" id="add-product1" checked>
-                                                        <label class="" for="add-product1"><span class="body-title-2">Allow</span></label>
-                                                    </div>
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="add-product" id="add-product2">
-                                                        <label class="" for="add-product2"><span class="body-title-2">Deny</span></label>
-                                                    </div>
-                                                </div>
-                                            </fieldset>
-                                            <fieldset class="mb-24">
-                                                <div class="body-title mb-10">Update product</div>
-                                                <div class="radio-buttons">
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="update-product" id="update-product1">
-                                                        <label class="" for="update-product1"><span class="body-title-2">Allow</span></label>
-                                                    </div>
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="update-product" id="update-product2" checked>
-                                                        <label class="" for="update-product2"><span class="body-title-2">Deny</span></label>
-                                                    </div>
-                                                </div>
-                                            </fieldset>
-                                            <fieldset class="mb-24">
-                                                <div class="body-title mb-10">Delete product</div>
-                                                <div class="radio-buttons">
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="delete-product" id="delete-product1" checked>
-                                                        <label class="" for="delete-product1"><span class="body-title-2">Allow</span></label>
-                                                    </div>
-                                                    <div class="item">
-                                                        <input class="" type="radio" name="delete-product" id="delete-product2">
-                                                        <label class="" for="delete-product2"><span class="body-title-2">Deny</span></label>
-                                                    </div>
-                                                </div>
-                                            </fieldset>
-                                            <fieldset class="mb-24">
-                                                <div class="body-title mb-10">Apply discount</div>
+                                                <div class="body-title mb-10">Account Status</div>
                                                 <div class="radio-buttons">
                                                     <div class="item">
                                                         <input class="" type="radio" name="apply-product" id="apply-product1">
-                                                        <label class="" for="apply-product1"><span class="body-title-2">Allow</span></label>
+                                                        <label class="" for="apply-product1"><span class="body-title-2">Active</span></label>
                                                     </div>
                                                     <div class="item">
                                                         <input class="" type="radio" name="apply-product" id="apply-product2" checked>
-                                                        <label class="" for="apply-product2"><span class="body-title-2">Deny</span></label>
+                                                        <label class="" for="apply-product2"><span class="body-title-2">Inactive</span></label>
+                                                    </div>
+                                                    <div class="item">
+                                                        <input class="" type="radio" name="apply-product" id="apply-product3" checked>
+                                                        <label class="" for="apply-product2"><span class="body-title-2">Banned</span></label>
                                                     </div>
                                                 </div>
                                             </fieldset>
                                             <fieldset class="">
-                                                <div class="body-title mb-10">Create coupon</div>
+                                                <div class="body-title mb-10">Role</div>
                                                 <div class="radio-buttons">
                                                     <div class="item">
                                                         <input class="" type="radio" name="create-product" id="create-product1">
-                                                        <label class="" for="create-product1"><span class="body-title-2">Allow</span></label>
+                                                        <label class="" for="create-product1"><span class="body-title-2">Admin</span></label>
                                                     </div>
                                                     <div class="item">
                                                         <input class="" type="radio" name="create-product" id="create-product2" checked>
-                                                        <label class="" for="create-product2"><span class="body-title-2">Deny</span></label>
+                                                        <label class="" for="create-product2"><span class="body-title-2">User</span></label>
                                                     </div>
                                                 </div>
                                             </fieldset>
