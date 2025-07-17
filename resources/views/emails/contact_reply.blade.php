3925e87403d79b65c75b24315b429fc6fe9d5638
HoaCoLau <thuanntph51789@gmail.com>
2025-07-17 10:09:56 +0700

    new3

---
+++ resources/views/emails/contact_reply.blade.php
@@ -0,0 +1,5 @@
+<p>Xin chào {{ $contact->name }},</p>
+<p>Admin đã phản hồi liên hệ của bạn:</p>
+<p><strong>Nội dung phản hồi:</strong></p>
+<p>{{ $contact->admin_reply }}</p>
+<p>Trân trọng,<br>Funori Team</p>
